cfb3db0e1bd45d9ded1efa29d20ed4ec2027f485
ahmed ouertani <ahmed.3m2@gmail.com>
2019-02-09 09:02:51 +0100

    ajout des input au formulaire

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmed
+ * Date: 08/02/2019
+ * Time: 22:10
+ */
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->
+        add('prenom')->add('tel')->remove('username');
+    }
+
+    public function getParent()
+    {
+        return BaseRegistrationFormType::class;
+    }
+
+}
