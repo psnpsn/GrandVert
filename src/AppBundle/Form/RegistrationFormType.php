87c08b43b31e91ad9f2dc379d8d66fc63521548c
ahmed ouertani <ahmed.3m2@gmail.com>
2019-02-11 19:46:13 +0100

    intégration de template

--- src/AppBundle/Form/RegistrationFormType.php
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
@@ -20,7 +21,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder ->add('avatar',FileType::class, ['label' => 'image avatar'])->add('nom')->
-        add('prenom')->add('tel')->remove('username');
+        add('prenom')->add('tel',TextType::class)->remove('username');
     }
 
     public function getParent()
